8db79f54acc6c18898eccfa1ab183fedc22c6bfa
davidpeva <davidpeva@gmail.com>
2023-07-04 11:44:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>HELLO I AM THE FIRST CONFIGURATION</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
