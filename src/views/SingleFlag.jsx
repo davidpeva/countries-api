2c828df6d8c5a4c8f6c74bcd3b18417f878f8ffd
davidpeva <davidpeva@gmail.com>
2023-07-27 14:57:58 -0500

    with go back button working but takes to the page 1

--- src/views/SingleFlag.jsx
+++ src/views/SingleFlag.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import axios from 'axios';
 
@@ -9,6 +9,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "../scss/common/_variables.scss";
 import "../scss/common/_global.scss";
+import "../scss/sections/_single-flag.scss";
 
 import { ThemeContext } from "../App";
 
@@ -19,6 +20,11 @@ export default function SingleFlag() {
 
   // HELP ME TO THE DYNAMIC LINK
   const params = useParams();
+
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  }
 
   //THIS IS FOR THE COLOR CHANGE
   const { theme } = useContext(ThemeContext);
@@ -86,101 +92,120 @@ export default function SingleFlag() {
   //ONCE I HAVE THE INDEX I USE IT
   const lastNativeCommon = info?.name?.nativeName[lastNativeNames].common;
 
+
+
   return (
     <main className="vh-100" id={theme}>
-      <div className="container">
+      <div className="container vh-100">
         <div className="px-3">
+          {/* BUTTON */}
           <div className="pt-4 pt-lg-5 d-lg-flex align-items-lg-center justify-content-lg-between">
-            <button className="back-button border-0 px-4 py-2 rounded">
+            <button className="back-button border-0 px-4 py-2 rounded" onClick={goBack}>
               <FontAwesomeIcon icon={faArrowLeft} className="pe-2" />
               Back
             </button>
           </div>
 
+          {/* INFO AND IMG */}
+          <div className="my-5 mb-lg-0 row gx-5">
 
-          {/* THE ? IS A CONDITIONAL RENDERING IS LIKE AND IF SI LO ENCUENTRA LO MUESTRA SI NO || no aplica */}
-          <div className="mt-5 row gx-5">
-
+            {/* IMG */}
             <div className="col-lg-6">
-              <img className="w-100" src={info?.flags.png} alt={info?.flags.alt} />
+              <img className="single-flag w-100" src={info?.flags.png} alt={info?.flags.alt} />
             </div>
 
-            <div className="col-lg-6">
-              <div className="mt-5 mt-lg-0">
-                <h2 className="fw-bold">{info?.name?.common || "N/A"}</h2>
+            {/* INFO */}
+            <div className="col-lg-6 my-lg-auto">
+
+              {/* ROW COL1 Y COL2 */}
+              <div className="row">
+                {/* COL1 INFO */}
+                <div className="col-lg-6">
+                  <div className="mt-5 mt-lg-0">
+                    <h2 className="fw-bold">{info?.name?.common || "N/A"}</h2>
+                  </div>
+                  <div className="mt-4">Native Name:
+                    <span className="fw-lighter ms-1">
+                      {lastNativeCommon || "N/A"}
+                    </span>
+                  </div>
+                  <div className="mt-2">Population:
+                    <span className="fw-lighter ms-1">
+                      {info?.population || "N/A"}
+                    </span>
+                  </div>
+                  <div className="mt-2">Region:
+                    <span className="fw-lighter ms-1">
+                      {info?.region || "N/A"}
+                    </span>
+                  </div>
+                  <div className="mt-2">Sub Region:
+                    <span className="fw-lighter ms-1">
+                      {info?.subregion || "N/A"}
+                    </span>
+                  </div>
+                  <div className="mt-2">Capital:
+                    <span className="fw-lighter ms-1">
+                      {info?.capital || "N/A"}
+                    </span>
+                  </div>
+                </div>
+
+
+                {/* COL 2 INFO */}
+                <div className="col-lg-6">
+                  <div className="mt-5">Top Level Domain:
+                    <span className="fw-lighter ms-1">
+                      {topLevelDomainString || "N/A"}
+                    </span>
+                  </div>
+                  <div className="mt-2">Currencies:
+                    <span className="fw-lighter ms-1">
+                      {currenciesNamesString}
+                    </span>
+                  </div>
+                  <div className="mt-2">Languages:
+                    <span className="fw-lighter ms-1">
+                      {languagesStrings}
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="mt-4">Native Name:
-                <span className="fw-lighter ms-1">
-                  {lastNativeCommon || "N/A"}
-                </span>
-              </div>
-              <div className="mt-2">Population:
-                <span className="fw-lighter ms-1">
-                  {info?.population || "N/A"}
-                </span>
-              </div>
-              <div className="mt-2">Region:
-                <span className="fw-lighter ms-1">
-                  {info?.region || "N/A"}
-                </span>
-              </div>
-              <div className="mt-2">Sub Region:
-                <span className="fw-lighter ms-1">
-                  {info?.subregion || "N/A"}
-                </span>
-              </div>
-              <div className="mt-2">Capital:
-                <span className="fw-lighter ms-1">
-                  {info?.capital || "N/A"}
-                </span>
-              </div>
-              <div className="mt-5">Top Level Domain:
-                <span className="fw-lighter ms-1">
-                  {topLevelDomainString || "N/A"}
-                </span>
-              </div>
-              <div className="mt-2">Currencies:
-                <span className="fw-lighter ms-1">
-                  {currenciesNamesString}
-                </span>
-              </div>
-              <div className="mt-2">Languages:
-                <span className="fw-lighter ms-1">
-                  {languagesStrings}
-                </span>
-              </div>
+
               {/* <div className="borders mt-5 d-lg-flex align-items-center">
-                <div className="w-lg-30">
+              <div className="w-lg-30">
+                Border Countries:
+              </div>
+              <div className="d-flex justify-content-between ms-lg-2 w-lg-70">
+                {
+                  bordersCountries
+                    ?
+                    bordersCountries.map(border => (
+                      <button key={border} className="borders-buttons border-0 px-4 py-2 rounded">
+                        {border}
+                      </button>
+                    ))
+                    :
+                    "N/A"
+                }
+              </div> */}
+
+              {/* MAPPING BUTTONS */}
+              <div className="borders mt-5 row align-items-center d-flex">
+                <div className="col-lg-3">
                   Border Countries:
                 </div>
-                <div className="d-flex justify-content-between ms-lg-2 w-lg-70">
+                <div className="col-lg-9 d-flex flex-wrap justify-content-lg-left">
                   {
                     bordersCountries
                       ?
                       bordersCountries.map(border => (
-                        <button key={border} className="borders-buttons border-0 px-4 py-2 rounded">
+                        <button key={border} className="buttons-width borders-buttons border-0 me-4 my-2 px-auto py-2 rounded">
                           {border}
                         </button>
                       ))
                       :
-                      "N/A"
-                  }
-                </div> */}
-              <div className="borders mt-5 row align-items-center">
-                <div className="col-12 col-lg-3">
-                  Border Countries:
-                </div>
-                <div className="col-12 col-lg-9 d-flex justify-content-lg-left">
-                  {
-                    bordersCountries
-                      ?
-                      bordersCountries.map(border => (
-                        <button key={border} className="borders-buttons border-0 me-4 px-4 py-2 rounded">
-                          {border}
-                        </button>
-                      ))
-                      :
-                      "N/A"
+                      <div>{"N/A"}</div>
                   }
                 </div>
 
